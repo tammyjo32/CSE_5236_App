5973b89c7a27721111ce47c6b19f6810bf3db1b3
Tammy <joseph.464@osu.edu>
2016-10-03 16:24:24 -0400

    adding files

---
+++ app/src/main/java/com/example/tammy/test/HelpActivity.java
@@ -0,0 +1,18 @@
+package com.example.tammy.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+
+    public void sendMessage(View view){
+
+    }
+}
---
+++ app/src/main/java/com/example/tammy/test/ProfileActivity.java
@@ -0,0 +1,36 @@
+package com.example.tammy.test;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private ImageView profileImageView;
+    private TextView usernameTextView;
+    private TextView emailTextVIew;
+
+    DatabaseHelper helper = new DatabaseHelper(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Intent intent = getIntent();
+        String email = intent.getStringExtra("Email");
+        String name = helper.getFirstName(email);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText("Hello, "+name);
+        textView.setTextColor(Color.parseColor("#FFFFFF"));
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_profile);
+        layout.addView(textView, 0);
+
+    }
+}
