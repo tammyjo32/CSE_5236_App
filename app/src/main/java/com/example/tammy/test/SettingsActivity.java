b53efaeb66f71e880e55eb15c80f295e7716150c
Tammy <joseph.464@osu.edu>
2016-10-03 17:45:09 -0400

    added switch. need to save settings

--- app/src/main/java/com/example/tammy/test/SettingsActivity.java
+++ app/src/main/java/com/example/tammy/test/SettingsActivity.java
@@ -3,13 +3,20 @@ package com.example.tammy.test;
 import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Switch accountSwitch;
+    private Switch notificationSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        accountSwitch = (Switch)findViewById(R.id.media_sites_settings);
+        notificationSwitch = (Switch)findViewById(R.id.discussion_notification_settings);
 
         setTitle("Settings");
     }
