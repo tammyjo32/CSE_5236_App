8019b70a5c532c0229bf16c58a4a7ae3290e34d5
Tammy <joseph.464@osu.edu>
2016-10-04 08:28:59 -0400

    Add edit icon on profile page, not implemented

--- app/src/main/java/com/example/tammy/test/ProfileActivity.java
+++ app/src/main/java/com/example/tammy/test/ProfileActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -33,6 +35,17 @@ public class ProfileActivity extends AppCompatActivity {
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_profile);
         layout.addView(textView, 0);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void editPage(MenuItem item) {
 
     }
 }
