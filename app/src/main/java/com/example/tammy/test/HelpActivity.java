5277502fc603d60fb1c3f8045f62835929b960ea
Tammy <joseph.464@osu.edu>
2016-10-04 18:51:38 -0400

    Contact us sends email

--- app/src/main/java/com/example/tammy/test/HelpActivity.java
+++ app/src/main/java/com/example/tammy/test/HelpActivity.java
@@ -1,8 +1,14 @@
 package com.example.tammy.test;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -14,7 +20,31 @@ public class HelpActivity extends AppCompatActivity {
         setTitle("Help");
     }
 
-    public void sendMessage(View view){
+    public void sendMessage(View view) {
+
+        EditText editText = (EditText)findViewById(R.id.subject);
+        String subject = editText.getText().toString();
+        EditText editText1 = (EditText)findViewById(R.id.message);
+        String message = editText1.getText().toString();
+
+        if(subject.equals("") || message.equals("")){
+            Toast temp = Toast.makeText(this, "Subject and Message field cannot be blank!" , Toast.LENGTH_SHORT);
+            temp.show();
+        }
+        else {
+
+            Intent email = new Intent(Intent.ACTION_SEND);
+            email.putExtra(Intent.EXTRA_EMAIL, new String[]{"tamtam1994@gmail.com"});
+            email.putExtra(Intent.EXTRA_SUBJECT, subject);
+            email.putExtra(Intent.EXTRA_TEXT, message);
+
+            email.setType("message/rfc822");
+
+            //Note that activity below will not work on emulator. Need real device to work
+            startActivity(Intent.createChooser(email, "Choose an Email Client :"));
+
+        }
 
     }
+
 }
